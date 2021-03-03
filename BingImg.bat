0bae40828786f7943b977ecce363ada75c1ae213
ZhangDezhi <winzdz@hotmail.com>
2021-03-03 20:03:53 +0800

    统一脚本编写

---
+++ BingImg.bat
@@ -0,0 +1,9 @@
+
+
+
+@ECHO OFF
+:; python3 __main__.py  ; exit $?
+:: TODO: 备份当前壁纸
+del C:\Users\hwzhao\Pictures\Bing\*.jpg
+python __main__.py
+
--- Py_BingImg.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-del C:\Users\hwzhao\Pictures\Bing\*.jpg
-python __main__.py
